8109bd7a03ee832f8ac1071b3b54d9d21453d246
snonpen <112386250+snonpen@users.noreply.github.com>
2022-10-23 13:04:10 +0300

    first commit

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Clear();
+Console.WriteLine("This program generates numbers from N to 1.");
+Console.WriteLine();
+int endValue=1;
+int numberN = EnterUserData("Enter the end number N:");
+if (numberN < endValue)
+{
+    numberN = endValue;
+}
+Console.Write(SumFunction(endValue, numberN));
+Console.WriteLine();
+
+int SumFunction(int endValue, int numberN)
+{
+    if (endValue == numberN)
+    {
+        return endValue;
+    }
+    Console.Write(SumFunction(endValue + 1,numberN) + ", ");
+    return endValue;
+}
+
+int EnterUserData(string nameUserData)
+{
+    Console.Write($"{nameUserData}");
+    return Convert.ToInt32(Console.ReadLine());
+}
